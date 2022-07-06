8d6e65a38a570e18be6292ea7ccebc0e2e2c5ba2
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-07-06 23:11:07 +0530

    adding rick search

---
+++ RickSearch_gettsup/ricksearch/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchAppBar from './components/SearchAppBar';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchAppBar/>
+    </div>
+  );
+}
+
+export default App;
